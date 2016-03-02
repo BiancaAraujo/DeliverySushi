fdab4da19b349d83d75fd68cf9c6d4a9caa03e76
BiancaAraujo <bianca12.vieira@gmail.com>
2016-03-02 10:09:42 -0400

    Update JanelaPagamento.java

--- DeliveryS/src/JanelaPagamento.java
+++ DeliveryS/src/JanelaPagamento.java
@@ -1,16 +1,92 @@
 
-public class JanelaPagamento {
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 
-	/*
-	 * Essa janela mostra uma tabela com os pedidos ainda não pagos (atributo status)
-	 * A última coluna tem uma coluna de botões (ButtonColumn)
-	 * 					(Ver em JanelaNotificacoes (trab engsoft) como usar isso. O que tem dentro dessa classe eu nunca entendi)
-	 * Ao clicar no botão, o programa realiza os seguintes passos:
-	 * 		1 - Atualizar pontos de fidelidade do cliente (gasto e ganho)
-	 * 		2 - Atualizar lucro (tabela Empresa do BD - dar um jeito para zerar todo dia)
-	 * 		3 - Atualizar popularidade dos itens comprados
-	 * 		4 - Mostrar ao usuário uma mensagem de sucesso
-	 * OBS: Vamos fazer verificação de entrada? O Verificacao.java (trab engsoft) faz algumas
-	 * */
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class JanelaPagamento extends JFrame{
 	
+	String code;
+	
+	private Container container;
+	
+	private JTable pedidos;
+    
+    // Para conectar com o BD
+ 	private ConnectionFactory con;
+ 	private Connection c;
+ 	ResultSet rs = null;	
+ 	ResultSet rs2 = null;
+ 	private void initConexao(){if(con == null){ con = new ConnectionFactory(); c = con.getConnection();}}
+
+    public JanelaPagamento()
+    {
+    	
+    	container = this.getContentPane();	 
+    	
+    	
+    	
+    	container.add(new JLabel("Pedidos"));
+		pedidos = pedidosAbertos("SELECT Pedido.pedido_Id, cpf, valor_total, forma_pagamento, entregador, status FROM Pedido WHERE status = '0'");
+		container.add(new JScrollPane(pedidos),BorderLayout.CENTER); 
+		
+		this.setLayout(new FlowLayout()); 
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setVisible(true);
+		this.setTitle("Pagamento"); 
+		this.setSize(800,800);    
+
+    	
+    }
+    
+    private JTable pedidosAbertos(String select) {
+		initConexao();		
+        try {
+        	//Prepara tabela
+        	 Object[] colunas = {"code", "CPF do cliente", "Valor", "Forma de Pagamento","Entregador", "Status", ""};  		
+			 DefaultTableModel model = new DefaultTableModel();		 
+			 model.setColumnIdentifiers(colunas); 		 
+			 Vector<Object[]> linhas = new Vector<Object[]>();
+			 
+        	 //Pesquisa no BD e monta tabela
+        	 PreparedStatement ptStatement = c.prepareStatement(select);
+	         rs = ptStatement.executeQuery();			 
+	         while (rs.next()){	        	 
+	        	 linhas.add(new Object[]{rs.getString("pedido_Id"), rs.getString("cpf"),rs.getString("valor_total"),rs.getString("forma_pagamento"),rs.getString("entregador"), rs.getString("status"), "Confirmar"}); 	           	
+	         }	         
+			 for (Object[] linha : linhas) {  
+		         model.addRow(linha);  
+		     }
+			 
+			 final JTable tarefasTable = new JTable();
+			 
+			 tarefasTable.setModel(model); 
+			 tarefasTable.setFillsViewportHeight(true);
+			 
+			 ButtonColumn buttonColumn = new ButtonColumn(tarefasTable, 6, "pedidosAbertos");
+
+			 
+	         return tarefasTable; 
+	         
+        } catch (SQLException ex) {
+            System.out.println("ERRO: " + ex);
+        }
+		return null; 
+	}
+
 }
